0456f68236bbf307e2215a797ecb86ff8c9c5ab2
peterbarnes <barnespeter213@gmail.com>
2015-01-31 15:55:08 -0600

    Update

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.navbar-dropdown-toggle-left').click(function() {
+    $('.menu-left').toggle(250);
+  });
+  $('.navbar-dropdown-toggle-right').click(function() {
+    $('.menu-right').toggle(250);
+  });
+});
